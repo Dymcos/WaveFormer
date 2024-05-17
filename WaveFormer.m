a92e0aa4b1d9b45c4642686eccb497dc6e177696
D.mon 24 <dchaminov@ya.ru>
2024-05-17 16:43:32 +0300

    waveformer methods to static

--- WaveFormer.m
+++ WaveFormer.m
@@ -1,26 +1,10 @@
 classdef WaveFormer
     %WaveFormer Creates two-dimensional time waveform out of resource matrix
-    %   the length of waveform is (rMatrix width * rMatrix lenght)
-
-    properties
-        resourceMatrix
-        tLenght
-        fLenght
-    end
-
-    methods
-        function obj = WaveFormer(resource_Matrix)
-            %UNTITLED Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.resourceMatrix = resource_Matrix;
-            obj.tLenght = length(obj.resourceMatrix(1,:)); %rMatrix horizontal size
-            obj.fLenght = length(obj.resourceMatrix(:,1)); %rMatrix vertical size
-        end
-        function waveform = createWaveform(obj)
-            waveform = zeros(1,obj.tLenght*obj.fLenght);
-            for k = 1:obj.tLenght
-            waveform(1+(k-1)*obj.fLenght:k*obj.fLenght) = ifft(obj.resourceMatrix(:,k));
-            end
+    %   the length of waveform is (rMatrix width * rMatrix lenght
+    methods(Static)
+        function waveform = createWaveform(resource_Matrix)
+            fLenght=length(resource_Matrix(:,1));
+            waveform=reshape(ifft(resource_Matrix,fLenght,2).',1,[]);
         end
     end
 end
