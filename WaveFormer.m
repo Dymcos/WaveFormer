71cc0421f4455d542dabd0ac8fc5a597e1716269
D.mon 24 <dchaminov@ya.ru>
2024-07-03 12:35:27 +0300

    Fix of complex-time signal

--- WaveFormer.m
+++ WaveFormer.m
@@ -1,10 +1,10 @@
 classdef WaveFormer
-    %WaveFormer Creates two-dimensional time waveform out of resource matrix
-    %   the length of waveform is (rMatrix width * rMatrix lenght
+    % WaveFormer Creates two-dimensional time waveform out of resource matrix
+    % the length of waveform is (rMatrix width * rMatrix lenght)
     methods(Static)
         function waveform = createWaveform(resource_Matrix)
             fLenght=length(resource_Matrix(:,1));
-            waveform=reshape(ifft(resource_Matrix,fLenght,2).',1,[]);
+            waveform=reshape(ifft(resource_Matrix,fLenght),1,[]);
         end
     end
 end
