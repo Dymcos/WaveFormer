4a6f3d9280ab3b31e1616935faa96fa5bd7faf22
D.mon 24 <dchaminov@ya.ru>
2024-07-22 15:47:10 +0300

    comments and more functionality

--- OfdmTransceiver.m
+++ OfdmTransceiver.m
@@ -1,12 +1,20 @@
 classdef OfdmTransceiver
     methods(Static)
-        function waveform = ResourceGrid2ComlexTime(resource_Matrix)
-            fLenght=length(resource_Matrix(:,1));
-            waveform=reshape(ifft(resource_Matrix,fLenght),1,[]);
+        function [waveform,symbs] = ResourceGrid2ComlexTime(resource_Matrix)
+            % transforms RG to comlex signal in time domain
+            % before using signal should be resampled
+            % fLenght=length(resource_Matrix(:,1));
+            symbs=ifft(resource_Matrix);
+            waveform=reshape(symbs,1,[]);
+            symbs=symbs.';
+            % ifft works with comlumns
         end
-        function resGrid=ComplexTime2ResourceGrid(samples,symb_length)
+        function resGrid = ComplexTime2ResourceGrid(samples,symb_length)
+            % transforms matched complex signal in time domain to RG
+            % starting at first sample of array
             samples=reshape(samples,symb_length,[]);
             resGrid=fft(samples,symb_length);
+            % fft works with columns
         end
     end
 end
